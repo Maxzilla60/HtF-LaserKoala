c661585c7a40f60d3e8208ee530ec0156dc19081
Maxime Orione <maxpower98@hotmail.com>
2017-11-30 09:26:38 +0100

    Initial Project

---
+++ HtF/src/main/java/com/laserkoala/HtF/HtFApplication.java
@@ -0,0 +1,12 @@
+package com.laserkoala.HtF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtFApplication.class, args);
+	}
+}
